d650d763c4a9c3c75da2e8a1da3ca48ffa2078ed
Riazanov Daniil Dmitrievich <drlitsey2@gmail.com>
2026-04-15 01:01:47 +0300

    Update hw45.py

--- part4_oop/hw45.py
+++ part4_oop/hw45.py
@@ -146,7 +146,7 @@ class CachedProperty[V]:
         self.func = func
         self.name = func.__name__
 
-    def __get__(self, instance: HasCache[Any, Any] | None, owner: type) -> V | "CachedProperty[V]":
+    def __get__(self, instance: HasCache[Any, Any] | None, owner: type) -> V:
         if instance is None:
             return self
 
