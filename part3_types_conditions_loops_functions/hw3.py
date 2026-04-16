447c5624671aa6300277df8344d6fbcc8e2f7932
Riazanov Daniil Dmitrievich <drlitsey2@gmail.com>
2026-04-16 15:09:41 +0300

    Update hw3.py

--- part3_types_conditions_loops_functions/hw3.py
+++ part3_types_conditions_loops_functions/hw3.py
@@ -25,8 +25,18 @@ EXPENSE_CATEGORIES: dict[str, tuple[str, ...]] = {
 }
 
 DAYS_IN_MONTH: tuple[int, ...] = (
-    31, 28, 31, 30, 31, 30,
-    31, 31, 30, 31, 30, 31
+    31,
+    28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
 )
 
 FEBRUARY: int = 2
